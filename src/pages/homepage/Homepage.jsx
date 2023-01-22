4b8620d16db77fbbd1a1be9218fd278889610225
northenligths <akash221002@gmail.com>
2023-01-22 19:44:58 +0530

    Started Project

---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../components/header/Header";
+import "./homepage.css";
+
+const Homepage = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default Homepage;
