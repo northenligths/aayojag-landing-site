8243602679896629ed07e9cf1ba5faf8072852f0
northenligths <akash221002@gmail.com>
2023-01-22 20:12:46 +0530

    Designed Header for mobile menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
+import logo from "../../assets/images/aayojag-logo-main.jpeg";
+import { AiOutlineMenu } from "react-icons/ai";
 
 const Header = () => {
-  return <div>Header</div>;
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    <div className="header bg-light d-flex justify-content-between shadow-sm py-2 px-2 align-items-center position-relative">
+      <div>
+        <img src={logo} alt="" className="mainLogo" />
+      </div>
+      <div className="d-md-none">
+        <AiOutlineMenu
+          className="menuIcon"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+        {menuOpen && (
+          <div className="bg-dark position-absolute menuListContainer">
+            <ul className="menuList d-flex flex-column justify-content-center align-items-center">
+              <li className="list-items">Home</li>
+              <li className="list-items">About</li>
+              <li className="list-items">Contact Us</li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Header;
